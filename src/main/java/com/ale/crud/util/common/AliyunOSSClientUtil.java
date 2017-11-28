dba71e3284b5792322060146768324cf355a31eb
alewu <wywujiawei@163.com>
2017-11-28 15:33:04 +0800

    refactor oss

--- src/main/java/com/ale/crud/util/common/AliyunOSSClientUtil.java
+++ src/main/java/com/ale/crud/util/common/AliyunOSSClientUtil.java
@@ -1,6 +1,5 @@
 package com.ale.crud.util.common;
 
-import com.ale.crud.common.response.MyResponse;
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.model.Bucket;
 import com.aliyun.oss.model.ObjectMetadata;
@@ -13,6 +12,8 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author alewu
@@ -31,100 +32,67 @@ public class AliyunOSSClientUtil {
     private static String accessKeyId = ConfigUtil.getParameter("accessKeyId");
     private static String accessKeySecret = ConfigUtil.getParameter("accessKeySecret");
 
-
     static {
         // 配置OSSClient（可选）
         // ClientConfiguration conf = new ClientConfiguration();
         ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
     }
 
-    /**
-     * 上传图片至OSS
-     *
-     * @param file   上传文
-     * @param folder 模拟文件夹名 如"qj_nanjing/"
-     * @return String 返回的唯一MD5数字签名
-     */
-    public static MyResponse uploadObject2OSS(MultipartFile file, String folder) {
-        MyResponse response = new MyResponse();
-        try (InputStream inputStream = file.getInputStream()) {
-            // 文件名
-            String fileName = file.getOriginalFilename();
-            if (fileName.endsWith(".apk")) {
-                fileName = "yzcx.apk";
-            }
-            fileName = fileName.substring(0, fileName.lastIndexOf(".")) + "_" +
-                    System.currentTimeMillis() + fileName.substring(fileName.lastIndexOf("."), fileName.length());
-            // 文件大小
-            Long fileSize = file.getSize();
-            // 创建上传Object的Metadata
-            ObjectMetadata metadata = new ObjectMetadata();
-            // 上传的文件的长度
-            metadata.setContentLength(inputStream.available());
-            // 指定该Object被下载时的网页的缓存行为
-            metadata.setCacheControl("no-cache");
-            // 指定该Object下设置Header
-            metadata.setHeader("Pragma", "no-cache");
-            // 指定该Object被下载时的内容编码格式
-            metadata.setContentEncoding("utf-8");
-            // 文件的MIME，定义文件的类型及网页编码，决定浏览器将以什么形式、什么编码读取文件。如果用户没有指定则根据Key或文件名的扩展名生成，
-            // 如果没有扩展名则填默认值application/octet-stream
-            metadata.setContentType(getContentType(fileName));
-            // 指定该Object被下载时的名称（指示MIME用户代理如何显示附加的文件，打开或下载，及文件名称）
-            metadata.setContentDisposition("filename/filesize=" + fileName + "/" + fileSize + "Byte.");
-            // 上传文件 (上传文件流的形式)
-            ossClient.putObject(bucketName, folder + fileName, inputStream, metadata);
-            // 解析结果
-            Date expiration = new DateTime().plusYears(10).toDate();
-            URL url = ossClient.generatePresignedUrl(bucketName, folder + fileName, expiration);
-            String urlString = url.toString();
-            String subUrl = urlString.substring(0, urlString.lastIndexOf("?"));
-            response.put("file", subUrl);
-            response.put("fileSize", Tools.getPrintSize(fileSize));
-            return response;
+    public static void main(String[] args) {
+        // 上传字符串
+        String content = "Hello OSS";
+        // key是文件名
+        ossClient.putObject(bucketName, "file", new ByteArrayInputStream(content.getBytes()));
+    }
 
+    /**
+     * 上传多个文件
+     * @param files 上传的文件
+     * @return 文件地址
+     */
+    public static List<String> uploadFiles(MultipartFile[] files) {
+        List<String> urls = new LinkedList<>();
+        for (MultipartFile file : files) {
+            String url = uploadFileToOSS(file);
+            urls.add(url);
+        }
+        ossClient.shutdown();
+        return urls;
+    }
+
+    /**
+     * 上传单个文件
+     * @param file 上传的文件
+     * @return 文件地址
+     */
+    public static String uploadFileToOSS(MultipartFile file) {
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        // 自定义文件名
+        fileName = fileName.substring(0, fileName.lastIndexOf(".")) + "_" +
+                System.currentTimeMillis() + fileName.substring(fileName.lastIndexOf("."), fileName.length());
+        // 根据文件类型获得文件保存的路径（bucket下文件路径）
+        String key = contentType + "/" + fileName;
+        try (InputStream inputStream = file.getInputStream()) {
+            int contentLength = inputStream.available();
+            ObjectMetadata metadata = getObjectMetadata(fileName, contentLength);
+            ossClient.putObject(bucketName, key, inputStream, metadata);
         } catch (Exception e) {
             e.printStackTrace();
             LOGGER.error("上传阿里云OSS服务器异常." + e.getMessage(), e);
-        } finally {
-            ossClient.shutdown();
         }
-        return null;
+        return getImgURL(key);
     }
 
-
-    /**
-     * 上传图片至OSS
-     * @param file 上传文件
-     * @param folder 模拟文件夹名 eg. "parent_dir/"
-     * @return 图片地址
-     */
-    public static String uploadObjectOSS(MultipartFile file, String folder) {
-        try (InputStream inputStream = file.getInputStream()) {
-            String fileName = file.getOriginalFilename();
-            ObjectMetadata metadata = getObjectMetadata(fileName);
-            // 上传的文件的长度
-            metadata.setContentLength(inputStream.available());
-            // 上传文件 (上传文件流的形式)
-            ossClient.putObject(bucketName, folder + fileName, inputStream, metadata);
-            // 解析结果,得到图片地址
-            Date expiresAt = new DateTime().plusYears(10).toDate();
-            return getImgURL(folder + fileName, expiresAt);
-        } catch (Exception e) {
-            e.printStackTrace();
-            LOGGER.error("上传阿里云OSS服务器异常." + e.getMessage(), e);
-        } finally {
-            ossClient.shutdown();
-        }
-        return null;
-    }
 
     /**
      * 设置对象/文件元信息
-     * @param fileName 文件名
-     * @return
+     *
+     * @param fileName      文件名
+     * @param contentLength 文件长度
+     * @return 对象/文件元信息
      */
-    public static ObjectMetadata getObjectMetadata(String fileName) {
+    public static ObjectMetadata getObjectMetadata(String fileName, int contentLength) {
         // 创建上传Object的Metadata
         ObjectMetadata metadata = new ObjectMetadata();
         // 指定该Object被下载时的网页的缓存行为
@@ -133,6 +101,8 @@ public class AliyunOSSClientUtil {
         metadata.setHeader("Pragma", "no-cache");
         // 指定该Object被下载时的内容编码格式
         metadata.setContentEncoding("utf-8");
+        // 必须设置ContentLength， 否则会报 411 missContentLength
+        metadata.setContentLength(contentLength);
         // 文件的MIME，定义文件的类型及网页编码，决定浏览器将以什么形式、什么编码读取文件。如果用户没有指定则根据Key或文件名的扩展名生成，
         // 如果没有扩展名则填默认值application/octet-stream
         metadata.setContentType(getContentType(fileName));
@@ -146,6 +116,7 @@ public class AliyunOSSClientUtil {
 
     /**
      * 通过文件名判断并获取OSS服务文件上传时文件的contentType
+     *
      * @param fileName 文件名
      * @return 文件的contentType
      */
@@ -188,35 +159,30 @@ public class AliyunOSSClientUtil {
 
 
     /**
-     * 根据key获取oss服务器上的图片地址
+     * 生成预签名URL
+     * 如果你想把自己的资源发放给第三方用户访问，但是又不想开放Bucket的读权限，可以通过生成预签名URL的形式提供给用户一个临时的访问URL。
+     * 在生成URL时，你可以指定URL过期的时间，从而限制用户长时间访问。
+     * 生成的URL默认以GET方式访问
+     *
      * @param key 文件名
      * @return 图片地址
      */
-    public static String getImgURL(String key, Date expiresAt) {
+    public static String getImgURL(String key) {
+        Date expiresAt = new DateTime().plusYears(10).toDate();
         URL url = ossClient.generatePresignedUrl(bucketName, key, expiresAt);
         String urlString = url.toString();
-        String subUrl = urlString.substring(0, urlString.lastIndexOf("?"));
-        ossClient.shutdown();
-        return subUrl;
-    }
-
-    public static void main(String[] args) {
-        // 上传字符串
-        String content = "Hello OSS";
-        // key是文件名
-        ossClient.putObject(bucketName, "file", new ByteArrayInputStream(content.getBytes()));
+        return urlString.substring(0, urlString.lastIndexOf("?"));
     }
 
     /**
      * 创建存储空间
-     * @param ossClient  阿里云OSS客户端对象
+     *
      * @param bucketName 存储空间名
      * @return
      */
-    public static boolean createBucket(OSSClient ossClient, String bucketName) {
+    public static boolean createBucket(String bucketName) {
         Bucket bucket = ossClient.createBucket(bucketName);
         return bucketName.equals(bucket.getName());
     }
-
 
 }
